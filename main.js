e038f6c6932912ea2e69df9a3080f55f12d1f0ee
Franco Aguzzi <faguzzi3@gmail.com>
2020-01-16 11:59:35 -0300

    rethinking variable scopes and readability

--- main.js
+++ main.js
@@ -6,55 +6,60 @@
     tableBody = document.querySelector(".tableBody"),
     selectArea = document.querySelector("#areaSelector"),
     selectGender = document.querySelector("#genderSelector");
-  let i = 0,
-    j = 0,
-    temporaryRow,
-    found = false,
-    columnIndexArea = 2,
-    columnIndexGender = 3;
 
   // SETTING DEFAULT VALUE FORMAT
   function getValueOnColumnSanitized(line, column) {
-    return tableBody.rows[line].cells[column].innerHTML.toUpperCase();
+    return tableBody.rows[line].cells[column].innerHTML.toUpperCase().trim();
   }
 
   // CREATING FILTER FUNCTIONS
-  function areaFilter() {
+  function areaFilter(line, column) {
     // Checks if the selected 'area' is in the 'area' cell
     if (selectArea.value.toUpperCase() === "ALL") return true;
     return (
-      getValueOnColumnSanitized(i, columnIndexArea) ===
-      selectArea.value.toUpperCase()
+      getValueOnColumnSanitized(line, column) === selectArea.value.toUpperCase()
     );
   }
 
-  function genderFilter() {
+  function genderFilter(line, column) {
     // Checks if the selected 'gender' is in the 'gender' cell
     if (selectGender.value.toUpperCase() === "ALL") return true;
     return (
-      getValueOnColumnSanitized(i, columnIndexGender) ===
+      getValueOnColumnSanitized(line, column) ===
       selectGender.value.toUpperCase()
     );
   }
 
-  function searchFilter() {
+  function searchFilter(line, column) {
     // Checks if user typed text is in the cell
     return (
-      getValueOnColumnSanitized(i, j).indexOf(searchInput.value.toUpperCase()) >
-      -1
+      getValueOnColumnSanitized(line, column).indexOf(
+        searchInput.value.toUpperCase()
+      ) > -1
     );
   }
 
   // USING ALL FILTERS SIMULTANEOUSLY
   function filterAll() {
+    debugger;
+    let i = 0, // row index
+      j = 0, // cell index
+      temporaryRow,
+      found = false,
+      columnIndexArea = 2, // area column index (used by area select filter)
+      columnIndexGender = 3; // gender column index (used by gender select filter)
+
     for (i; i < tableBody.rows.length; i++) {
       // Iterates on table rows
       temporaryRow = tableBody.rows[i]; // Stores temporary row
-      compareTexts: if (genderFilter() && areaFilter()) {
+      compareTexts: if (
+        genderFilter(i, columnIndexGender) &&
+        areaFilter(i, columnIndexArea)
+      ) {
         // Only uses search filter if candidate already respected 'area' and 'gender' filters
         for (j; j < temporaryRow.cells.length; j++) {
           // Iterates on temporary row cells
-          if (searchFilter()) {
+          if (searchFilter(i, j)) {
             found = true;
             break compareTexts; // Breaks out of the row (already contains everything the user searched)
           }
