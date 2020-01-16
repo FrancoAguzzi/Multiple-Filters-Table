aecdf2e1a7cc9ef921348b527660eca090add0aa
Franco Aguzzi <faguzzi3@gmail.com>
2020-01-16 10:51:34 -0300

    updating code logic

--- main.js
+++ main.js
@@ -9,96 +9,63 @@
   let i = 0,
     j = 0,
     temporaryRow,
-    found = false;
+    found = false,
+    columnIndexArea = 2,
+    columnIndexGender = 3;
+
+  // SETTING DEFAULT VALUE FORMAT
+  function getValueOnColumnSanitized(line, column) {
+    return tableBody.rows[line].cells[column].innerHTML.toUpperCase();
+  }
 
   // CREATING FILTER FUNCTIONS
   function areaFilter() {
     // Checks if the selected 'area' is in the 'area' cell
+    if (selectArea.value.toUpperCase() === "ALL") return true;
     return (
-      tableBody.rows[i].cells[2].innerHTML.toUpperCase() ===
+      getValueOnColumnSanitized(i, columnIndexArea) ===
       selectArea.value.toUpperCase()
     );
   }
 
   function genderFilter() {
     // Checks if the selected 'gender' is in the 'gender' cell
+    if (selectGender.value.toUpperCase() === "ALL") return true;
     return (
-      tableBody.rows[i].cells[3].innerHTML.toUpperCase() ===
+      getValueOnColumnSanitized(i, columnIndexGender) ===
       selectGender.value.toUpperCase()
     );
   }
 
   function searchFilter() {
-    // Checks if user text input is in the cell
+    // Checks if user typed text is in the cell
     return (
-      tableBody.rows[i].cells[j].innerHTML
-        .toUpperCase()
-        .trim()
-        .indexOf(searchInput.value.toUpperCase()) > -1
+      getValueOnColumnSanitized(i, j).indexOf(searchInput.value.toUpperCase()) >
+      -1
     );
   }
 
   // USING ALL FILTERS SIMULTANEOUSLY
   function filterAll() {
-    if (
-      selectArea.value.toUpperCase() !== "ALL" &&
-      selectGender.value.toUpperCase() !== "ALL" // Using 3 filters
-    ) {
-      for (i; i < tableBody.rows.length; i++) {
-        // Iterates on table rows
-        temporaryRow = tableBody.rows[i]; // Stores temporary row
-        compareTexts: for (j; j < temporaryRow.cells.length; j++) {
-          // Iterates on temporary row cells
-          if (searchFilter() && genderFilter() && areaFilter()) {
-            found = true;
-            break compareTexts; // Breaks out of the row (contains all the user searched)
-          }
-        }
-        temporaryRow.style.display = found ? "" : "none"; // if something wasn't found = sets line display to none
-        // Reset variables
-        found = false;
-        j = 0;
-      }
-      i = 0;
-    } else if (
-      selectArea.value.toUpperCase() === "ALL" &&
-      selectGender.value.toUpperCase() === "ALL" // Using 1 filter (search)
-    ) {
-      for (i; i < tableBody.rows.length; i++) {
-        // Iterates on table rows
-        temporaryRow = tableBody.rows[i]; // Stores temporary row
-        compareTexts: for (j; j < temporaryRow.cells.length; j++) {
+    for (i; i < tableBody.rows.length; i++) {
+      // Iterates on table rows
+      temporaryRow = tableBody.rows[i]; // Stores temporary row
+      compareTexts: if (genderFilter() && areaFilter()) {
+        // Only uses search filter if candidate already respected 'area' and 'gender' filters
+        for (j; j < temporaryRow.cells.length; j++) {
           // Iterates on temporary row cells
           if (searchFilter()) {
             found = true;
-            break compareTexts; // Breaks out of the row (already contains input value)
+            break compareTexts; // Breaks out of the row (already contains everything the user searched)
           }
         }
-        temporaryRow.style.display = found ? "" : "none"; // if input value not found = sets line display to none
-        // Reset variables
-        found = false;
-        j = 0;
       }
-      i = 0;
-    } else {
-      // Using 2 filters
-      for (i; i < tableBody.rows.length; i++) {
-        // Iterates on table rows
-        temporaryRow = tableBody.rows[i]; // Stores temporary row
-        compareTexts: for (j; j < temporaryRow.cells.length; j++) {
-          // Iterates on temporary row cells
-          if (searchFilter() && (genderFilter() || areaFilter())) {
-            found = true;
-            break compareTexts; // Breaks out of the row (already contains input value)
-          }
-        }
-        temporaryRow.style.display = found ? "" : "none"; // if something wasn't found = sets line display to none
-        // Reset variables
-        found = false;
-        j = 0;
-      }
-      i = 0;
+      temporaryRow.style.display = found ? "" : "none"; // if something wasn't found = sets line display to none
+      // Reset variables
+      found = false;
+      j = 0;
     }
+    i = 0;
   }
 
   // ADD EVENT LISTENERS
